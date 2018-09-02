fb684697dc9af6a69252771a71a29685eb45eb5f
stanko <stanko@gmail.com>
2018-09-02 12:15:51 +0200

    User roles and auth

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -19,6 +19,7 @@ class UsersTableSeeder extends Seeder
         $admin = new User();
         $admin->name = 'Admin Name';
         $admin->email = 'admin@email.com';
+        $admin->type = ' ';
         $admin->password = bcrypt('admin123');
         $admin->save();
         $admin->roles()->attach($role_admin);
@@ -26,6 +27,7 @@ class UsersTableSeeder extends Seeder
         $user = new User();
         $user->name = 'User Name';
         $user->email = 'user@email.com';
+        $user->type = ' ';
         $user->password = bcrypt('user123');
         $user->save();
         $user->roles()->attach($role_user);
